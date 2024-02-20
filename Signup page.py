66c9222f60b957fc89e521e6f3e60b622325d5aa
hamda hassan <hamda6464@gmail.com>
2024-02-20 21:37:45 +0000

    done 20/02/24

--- Signup page.py
+++ Signup page.py
@@ -13,7 +13,10 @@ root= customtkinter.CTk()
 root.geometry("700x700")
 
 frame = customtkinter.CTkFrame(master=root)
-frame.pack(padx=20, pady=70, fill="both", expand="true")
+frame.pack(padx=20, pady=70, fill="both", expand=True)
+
+firstname = customtkinter.CTkEntry(master=frame, placeholder_text="First Name")
+firstname.pack(padx=12 , pady=10)
 
 #name = customtkinter.CTkLabel()
 
