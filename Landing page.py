459e248f0ef537f378187073e6f1e40f107e55b4
hamda hassan <hamda6464@gmail.com>
2024-03-16 20:33:17 +0000

    16.03.2024

--- Landing page.py
+++ Landing page.py
@@ -15,4 +15,7 @@ root.geometry("500x350")
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
+loginbtn = customtkinter.CTkButton(master=root, text="LOGIN HERE", command=gologin)
+loginbtn.pack(padx=12, pady=10)
+
 
