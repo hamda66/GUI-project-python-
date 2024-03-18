dac95e794b8ea86af97d3a371a04002c0bccccbf
hamda hassan <hamda6464@gmail.com>
2024-03-18 21:11:09 +0000

    18-03

--- Landing page.py
+++ Landing page.py
@@ -8,16 +8,27 @@ customtkinter.set_appearance_mode("light")
 
 customtkinter.set_default_color_theme("green")
 
+def gologin():
+    print("do nothing")
+
+def signuppage():
+    print("do nothing")
+
+
 root = customtkinter.CTk()
 
-root.geometry("500x350")
+root.geometry("300x150")
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-loginbtn = customtkinter.CTkButton(master=root, text="LOGIN HERE", command=gologin)
+loginbtn = customtkinter.CTkButton(master=frame, text="LOGIN HERE", command=gologin)
 loginbtn.pack(padx=12, pady=10)
 
-signup = customtkinter.CTkButton(master=root, text="SIGN UP HERE", command=signuppage)
+signup = customtkinter.CTkButton(master=frame, text="SIGN UP HERE", command=signuppage)
 signup.pack(padx=12,pady=10)
+
+root.mainloop()
+
+
 
